095abec448ca245f5cdfa4bef5152c2fedba7c19
Zeidev <razelkull@gmail.com>
2021-10-19 05:02:41 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-branch=master
+branch=main
 git clone -b $branch https://github.com/Zeidev/MirroX-V2 /usr/src/app
 cd /usr/src/app
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t bot
